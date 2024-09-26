444b10d3dda7099d0049bf2d713d6c1696287d83
heajun jeong <heajun35@gmail.com>
2024-09-26 20:35:15 +0900

    [Feat] - 백엔드 소켓 연결 해체 시 console 작성

--- backend/util/io.js
+++ backend/util/io.js
@@ -1,6 +1,10 @@
 function socket(io) {
   io.on("connection", async (socket) => {
     console.log("새로운 유저가 접속했습니다.", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("유저가 접속을 종료했습니다.");
+    });
   });
 }
 
