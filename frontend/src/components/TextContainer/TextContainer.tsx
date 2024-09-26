d1f0f68817ba2ab7104d7b9df1c4d24c5bb441b8
heajun jeong <heajun35@gmail.com>
2024-09-26 23:29:41 +0900

    [Feat] - 설명 정리

--- frontend/src/components/TextContainer/TextContainer.tsx
+++ frontend/src/components/TextContainer/TextContainer.tsx
@@ -18,13 +18,13 @@ function TextContainer({ users }: { users: User[] }) {
           </span>
         </h1>
         <h2>
-          Created with React, Express, Node and Socket.IO{" "}
+          똑같은 문구를 보낼 수 없습니다.
           <span role="img" aria-label="emoji">
             ❤️
           </span>
         </h2>
         <h2>
-          Try it out right now!{" "}
+          간단히 구현한 것으로 문제가 많습니다.
           <span role="img" aria-label="emoji">
             ⬅️
           </span>
